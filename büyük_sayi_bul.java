3bb778332a444d80a6363e17854a64eb606fd2f2
aliaslan01 <aslanmuhammedali047@gmail.com>
2023-08-02 15:53:44 +0300

    BuyukSayiBul
    Java ile iki sayıdan büyük olanı bulma projesi

---
+++ büyük_sayi_bul.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vize_hazirlik;
+
+/**
+ *
+ * @author aslan
+ */
+public class büyük_sayi_bul {
+
+    
+    public static void main(String[] args) {
+        
+        int a = 4;
+        int b = 5;
+        
+        if(a>b ){
+            System.out.println("A büyüktür");
+        }
+        if(b>a){
+            System.out.println("B büyüktür");
+        }
+        
+    }
+    
+}
